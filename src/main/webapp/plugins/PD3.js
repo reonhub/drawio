901cbb50e6a716876cb4f3fa58200ba691dc37d2
jumpeeeeenta <j_goto@itra.co.jp>
2020-12-10 22:44:27 +0900

    set parent process function

--- src/main/webapp/plugins/PD3.js
+++ src/main/webapp/plugins/PD3.js
@@ -1673,7 +1673,7 @@ Actions.prototype.init = function () {
 
 
 Menus.prototype.addPopupMenuStyleItems = function (a, c, d) {
-  1 == this.editorUi.editor.graph.getSelectionCount() ? this.addMenuItems(a, ["-", "setAsDefaultStyle", "-", "setContainerIntheSameLayer", "-", "setParentProcessIntheSameLayer"], null, d) : this.editorUi.editor.graph.isSelectionEmpty() && this.addMenuItems(a, ["-", "clearDefaultStyle"], null, d)
+  1 == this.editorUi.editor.graph.getSelectionCount() ? this.addMenuItems(a, ["-", "setAsDefaultStyle", "-", "setContainerIntheSameLayer", "setParentProcessIntheSameLayer"], null, d) : this.editorUi.editor.graph.isSelectionEmpty() && this.addMenuItems(a, ["-", "clearDefaultStyle"], null, d)
 };
 
 (function () {
@@ -2024,37 +2024,38 @@ Menus.prototype.addPopupMenuStyleItems = function (a, c, d) {
 
     q.setParentProcess_sameLayer = function (b, c) {
 
-      var selectedCell = this.getSelectionCell();
+      var selectedContainer = this.getSelectionCell();
 
-      var selectedCell_x = selectedCell.geometry.x;
-      var selectedCell_y = selectedCell.geometry.y;
+      var selectedContainer_x = selectedContainer.geometry.x;
+      var selectedContainer_y = selectedContainer.geometry.y;
 
-      var cell = new mxCell(selectedCell.value, new mxGeometry(0, 0, 200, 200), 'swimlane;pd3type=container;');
+      var cell = new mxCell(selectedContainer.value, new mxGeometry(0, 0, 120, 60), 'rounded=0;whiteSpace=wrap;pd3type=action;');
       cell.vertex = true;
       var edge = new mxCell('', new mxGeometry(0, 0, 0, 0), 'html=1;verticalAlign=bottom;endArrow=block;endSize=8;entryX=0;entryY=1;exitX=0;exitY=0;edgeStyle=orthogonalEdgeStyle;pd3type=arrow;');
 
-      cell.geometry.x = selectedCell_x;
-      cell.geometry.y = selectedCell_y + selectedCell.geometry.height + 60;
+      cell.geometry.x = selectedContainer_x;
+      cell.geometry.y = selectedContainer_y - 120;
 
-      edge.geometry.setTerminalPoint(new mxPoint(selectedCell_x, cell.geometry.y), true);
-      edge.geometry.setTerminalPoint(new mxPoint(selectedCell_x, selectedCell_y+selectedCell.geometry.height), false);
+      edge.geometry.setTerminalPoint(new mxPoint(selectedContainer_x, selectedContainer_y), false);
+
+      edge.geometry.setTerminalPoint(new mxPoint(selectedContainer_x, selectedContainer_y-60), true);
       edge.edge = true;
 
-      cell.insertEdge(edge, true);
-      selectedCell.insertEdge(edge, false);
+      cell.insertEdge(edge, false);
+      selectedContainer.insertEdge(edge, true);
 
-      var selectedCell_style = [];
+      var selectedContainer_style = [];
       var pd3layer_source = "",
           fillColor_source = "",
           strokeColor_source = "";
-          selectedCell_style = selectedCell.style.split(';');
-      for (var i = 0; i < selectedCell_style.length; i++) {
-        if (selectedCell_style[i].indexOf('pd3layer') !== -1) {
-          pd3layer_source = selectedCell_style[i]+';';
-        } else if (selectedCell_style[i].indexOf('fillColor') !== -1) {
-          fillColor_source = selectedCell_style[i]+';';
-        } else if (selectedCell_style[i].indexOf('strokeColor') !== -1) {
-          strokeColor_source = selectedCell_style[i] + ';';
+          selectedContainer_style = selectedContainer.style.split(';');
+      for (var i = 0; i < selectedContainer_style.length; i++) {
+        if (selectedContainer_style[i].indexOf('pd3layer') !== -1) {
+          pd3layer_source = selectedContainer_style[i]+';';
+        } else if (selectedContainer_style[i].indexOf('fillColor') !== -1) {
+          fillColor_source = selectedContainer_style[i]+';';
+        } else if (selectedContainer_style[i].indexOf('strokeColor') !== -1) {
+          strokeColor_source = selectedContainer_style[i] + ';';
         }
       }
       
